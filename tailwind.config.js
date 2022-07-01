583d8f6c3ae324ea7478200ca2b4dc99d0576753
rynhnAtmdja <dealguein123@gmail.com>
2022-07-01 22:40:35 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#059669",
+        secondary: "#0f172a",
+        tertiary: "",
+      },
+      fontFamily: {},
+    },
+  },
+  plugins: [],
+};
