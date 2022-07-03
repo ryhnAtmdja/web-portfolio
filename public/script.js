0f5ddddb8ceea84a9d3ccc12619e84f9c86d7697
rynhnAtmdja <dealguein123@gmail.com>
2022-07-03 12:42:56 +0700

    navbar untk desktop ver

--- public/script.js
+++ public/script.js
@@ -1,5 +1,18 @@
+window.onscroll = function () {
+  const nav = document.getElementById("nav");
+  const fixedNav = nav.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    nav.classList.add("nav-scrolled");
+  } else {
+    nav.classList.remove("nav-scrolled");
+  }
+};
+
 const hmgBtn = document.getElementById("hamburger");
+const navlists = document.getElementById("navlists");
 
 hmgBtn.addEventListener("click", function () {
   hmgBtn.classList.toggle("hamburger-active");
+  navlists.classList.toggle("hidden");
 });
